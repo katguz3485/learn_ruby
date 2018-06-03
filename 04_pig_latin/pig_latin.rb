c2d78697ca3d05b0cfae5b4c6ffdda9f53e811bf
Your Name <you@example.com>
2018-06-03 11:44:35 +0200

    initial commit

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,16 @@
-#write your code here
+def translate(words)
+  s = words.split
+  pig_latin = []
+  for word in s
+    vowel_index = word.index(/[aeiou]/)
+    if  vowel_index == 0
+      word = word + "ay"
+    elsif word.match("qu")
+      word.replace word[vowel_index+1..-1] + word[0..vowel_index] + "ay"
+    else
+      word.replace word[vowel_index..-1] + word[0..vowel_index-1] + "ay"
+    end
+    pig_latin << word
+  end
+  return pig_latin.join(" ")
+end
