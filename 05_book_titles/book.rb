0387516c0b1eac7c215dddc2abdd43e4b3db4dff
Your Name <you@example.com>
2018-06-03 12:03:11 +0200

    book titles done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    words = @title.split.map do |word|
+      (%w(the in of an a over).include?(word)) ? word : word.capitalize
+    end
+    words[0] = words.first.capitalize
+  end
 end
